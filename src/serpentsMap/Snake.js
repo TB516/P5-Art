a3cc7cafd33dbf26b76ed3ad344dbe6e0469a707
Thomas <77514919+TB516@users.noreply.github.com>
2025-02-02 03:36:33 -0500

    Commented serpents map and modified body position to be tighter random

--- src/serpentsMap/Snake.js
+++ src/serpentsMap/Snake.js
@@ -2,68 +2,102 @@ import { Position } from "../utils/Position.js";
 
 export class Snake {
   /**
+   * List of snake body positions.
    * @type {Position[]}
    * @readonly
    */
   body;
   /**
+   * Number of body cells in the snake.
    * @type {number}
    * @readonly
    */
   bodyLength;
   /**
+   * Current size of the body.
    * @type {number}
    */
   bodySize;
   /**
+   * Maximum value of random function for offsetting the body from the head.
+   * @type {number}
+   * @readonly
+   */
+  bodyOffsetMin;
+  /**
+   * Minimum value of random function for offsetting the body from the head.
+   * @type {number}
+   * @readonly
+   */
+  bodyOffsetMax;
+  /**
+   * Minimum size of the snake.
    * @type {number}
    * @readonly
    */
   minSize;
   /**
+   * Size to change the snake by.
+   * @type {number}
+   * @readonly
+   */
+  deltaSize;
+  /**
+   * Time to shrink the snake by delta size.
    * @type {number}
    * @readonly
    */
   shrinkTime;
   /**
+   * Value to scale non color perlin noise by.
    * @type {number}
    * @readonly
    */
   noiseScale;
   /**
+   * Mean value for gaussian random.
    * @type {number}
    * @readonly
    */
   gaussianMean;
   /**
+   * Standard deviation for gaussian random.
    * @type {number}
    * @readonly
    */
   gaussianSd;
   /**
+   * Value to scale the color perlin noise by.
    * @type {number}
    * @readonly
    */
   hueScale;
 
   /**
-   * @param {number} startingX
-   * @param {number} startingY
-   * @param {number} bodyLength
-   * @param {number} [startingSize=50]
-   * @param {number} [minSize=2]
-   * @param {number} [shrinkTime=60]
-   * @param {number} [noiseScale=100]
-   * @param {number} [gaussianMean=0]
-   * @param {number} [gaussianSd=10]
-   * @param {number} [hueScale=360]
+   * Creates a new snake with the requested parameters.
+   * @param {number} startingX Starting X position of snake.
+   * @param {number} startingY Starting Y position if snake.
+   * @param {number} bodyLength Number of body pieces snake will have.
+   * @param {number} [startingSize] Size of snake body pieces when program starts. Default of 25.
+   * @param {number} [bodyPosOffsetMax] Max range of random offset to snake body. Default of 1.
+   * @param {number} [bodyPosOffsetMin] Min range of random offset to snake body. Default of -1.
+   * @param {number} [minSize] Minimum size of the snake if shrinking is active. Default of 2.
+   * @param {number} [deltaSize] Change in snake size after shrink time has elapsed. Default of 0.
+   * @param {number} [shrinkTime] Number of frames it takes for the body to shrink one size. Default of 60.
+   * @param {number} [noiseScale] Scale value for perlin noise. Default of 100.
+   * @param {number} [gaussianMean] Mean of gausian random. Default of 0.
+   * @param {number} [gaussianSd] Standard deviation of gausian random. Default of 10.
+   * @param {number} [hueScale] Value hue is scaled by. Default of 360.
    */
   constructor(
     startingX,
     startingY,
     bodyLength,
     startingSize = 25,
+    bodyPosOffsetMax = 1,
+    bodyPosOffsetMin = -1,
     minSize = 2,
+    deltaSize = 0,
     shrinkTime = 60,
     noiseScale = 100,
     gaussianMean = 0,
@@ -72,6 +106,9 @@ export class Snake {
   ) {
     this.bodyLength = bodyLength;
     this.bodySize = startingSize;
+    this.bodyOffsetMax = bodyPosOffsetMax;
+    this.bodyOffsetMin = bodyPosOffsetMin;
+    this.deltaSize = deltaSize;
     this.minSize = minSize;
     this.shrinkTime = shrinkTime;
     this.noiseScale = noiseScale;
@@ -87,9 +124,10 @@ export class Snake {
   }
 
   /**
-   * @param {number} frameCount
-   * @param {number} canvasWidth
-   * @param {number} canvasHeight
+   * Updates snake position.
+   * @param {number} frameCount Current frame count of sketch.
+   * @param {number} canvasWidth Width of canvas.
+   * @param {number} canvasHeight Height of canvas.
    */
   update(frameCount, canvasWidth, canvasHeight) {
     let posStorage = new Position(this.body[0].x, this.body[0].y);
@@ -117,24 +155,32 @@ export class Snake {
     if (this.body[0].y < 0) this.body[0].y += canvasHeight;
     //#endregion
 
-    //Update tail
+    //#region Update tail position
     for (let i = 1; i < this.bodyLength; ++i) {
       const posStorage2 = new Position(this.body[i].x, this.body[i].y);
 
       this.body[i].x =
-        posStorage.x + randomGaussian(this.gaussianMean, this.gaussianSd);
+        posStorage.x + random(this.bodyOffsetMin, this.bodyOffsetMax + 1);
       this.body[i].y =
-        posStorage.y + randomGaussian(this.gaussianMean, this.gaussianSd);
+        posStorage.y + random(this.bodyOffsetMin, this.bodyOffsetMax + 1);
 
       posStorage.x = posStorage2.x;
       posStorage.y = posStorage2.y;
     }
+    //#endregion
 
-    if (frameCount % this.shrinkTime === 0 && this.bodySize > this.minSize) {
-      this.bodySize -= this.minSize;
+    //Shrink snake
+    if (
+      frameCount % this.shrinkTime === 0 &&
+      this.bodySize - this.deltaSize >= this.minSize
+    ) {
+      this.bodySize -= this.deltaSize;
     }
   }
 
+  /**
+   * Draws the snake to the screen.
+   */
   draw() {
     for (let i = 0; i < this.bodyLength; ++i) {
       fill(
--- src/serpentsMap/sketch.js
+++ src/serpentsMap/sketch.js
@@ -1,45 +1,65 @@
 import { Snake } from "./Snake.js";
 
 /**
+ * List of snakes in the sketch.
  * @type {Snake[]}
  */
 const snakes = [];
 
 /**
- * Framerate of sim
+ * Target framerate of sketch.
  * @type {number}
  */
 const framerate = 60;
 
 /**
- * Time in seconds until reset
+ * Time in seconds until reset.
  * @type {number}
  */
-const duration = 180;
+const duration = 60;
 
 /**
+ * Number of snakes in the sketch.
  * @type {number}
  */
 let snakeCount;
 
+/**
+ * Initializes the sketch variables and sets canvas size.
+ */
 const init = () => {
   createCanvas(windowWidth, windowHeight);
   background(0);
   colorMode(HSB, 360, 100, 100);
 
+  frameCount = 0;
   frameRate(framerate);
 
   snakeCount = 100;
   snakes.length = 0;
+
   for (let i = 0; i < snakeCount; ++i) {
     snakes.push(
-      new Snake(width / 2, height / 2, 15, 8, 2, duration * framerate * (3 / 5))
+      new Snake(
+        width / 2,
+        height / 2,
+        15,
+        8,
+        1,
+        -1,
+        1,
+        1,
+        duration * framerate * (3 / 5)
+      )
     );
   }
 
   noStroke();
 };
 
+/**
+ * Re-randomizes seeds then calls init.
+ */
 const reSeededInit = () => {
   randomSeed(Date.now());
   noiseSeed(Date.now());
@@ -47,10 +67,16 @@ const reSeededInit = () => {
   init();
 };
 
+/**
+ * Setup function called when page is first loaded.
+ */
 function setup() {
   init();
 }
 
+/**
+ * Updates and draws one frame of the sketch.
+ */
 function draw() {
   for (let i = 0; i < snakeCount; ++i) {
     snakes[i].update(frameCount, width, height);
@@ -62,6 +88,9 @@ function draw() {
   }
 }
 
+/**
+ * Re-initializes the sketch when the window is resized.
+ */
 function windowResized() {
   init();
 }
