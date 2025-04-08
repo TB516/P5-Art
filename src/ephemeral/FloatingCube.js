3a67b16ceb6f02d2cfe835aae18006f160df8392
Thomas <77514919+TB516@users.noreply.github.com>
2025-04-08 00:52:32 -0400

    Create FloatingCube.js

---
+++ src/ephemeral/FloatingCube.js
@@ -0,0 +1,33 @@
+import { Cube } from "../utils/Cube.js";
+
+export class FloatingCube {
+  /**
+   * @type {Cube}
+   */
+  cube;
+
+  /**
+   * Creates a new cube with the given values.
+   * @param {number} x X position.
+   * @param {number} y Y position.
+   * @param {number} z Z position.
+   * @param {number} length Length of cube (X axis).
+   * @param {number} width Width of cube (Z axis).
+   * @param {number} height Height of cube (Y axis).
+   */
+  constructor(x, y, z, length, width, height) {
+    this.cube = new Cube(x, y, z, length, width, height);
+  }
+
+  draw() {
+    push();
+    fill(0);
+    translate(
+      this.cube.position.x + this.cube.length / 2,
+      this.cube.position.y + this.cube.height / 2,
+      this.cube.position.z + this.cube.width / 2
+    );
+    box(this.cube.length, this.cube.height, this.cube.width);
+    pop();
+  }
+}
