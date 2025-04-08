9f89c73528e5038106ce356a38ec34e4d0dae6a0
Thomas <77514919+TB516@users.noreply.github.com>
2025-04-07 22:51:25 -0400

    Created position 3 class

---
+++ src/utils/Position3.js
@@ -0,0 +1,24 @@
+export class Position3 {
+  /**
+   * @type {number}
+   */
+  x;
+  /**
+   * @type {number}
+   */
+  y;
+  /**
+   * @type {number}
+   */
+  z;
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
