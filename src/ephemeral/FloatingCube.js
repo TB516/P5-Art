da4402e314631b04a51b54ff7572502471ada152
Thomas <77514919+TB516@users.noreply.github.com>
2025-04-08 03:39:12 -0400

    Completed ephemeral

--- src/ephemeral/FloatingCube.js
+++ src/ephemeral/FloatingCube.js
@@ -1,10 +1,16 @@
 import { Cube } from "../utils/Cube.js";
+import { Vector3 } from "../utils/Vector3.js";
 
 export class FloatingCube {
   /**
    * @type {Cube}
    */
   cube;
+
+  /**
+   * @type {number}
+   */
+  baseWidth;
 
   /**
    * Creates a new cube with the given values.
@@ -16,7 +22,40 @@ export class FloatingCube {
    * @param {number} height Height of cube (Y axis).
    */
   constructor(x, y, z, length, width, height) {
+    this.baseWidth = 1;
     this.cube = new Cube(x, y, z, length, width, height);
+  }
+
+  /**
+   * Updates the height of the cube based on proximity to the light.
+   * @param {Vector3[]} lights Light source positions
+   */
+  update(lights) {
+    let totalWidth = this.baseWidth;
+
+    for (const light of lights) {
+      totalWidth += this.calculateWidthContribution(light);
+    }
+
+    this.cube.width = totalWidth;
+  }
+
+  /**
+   * Calculates the width contribution of a single light source.
+   * @param {Vector3} light Position of the light source
+   * @returns {number} Width contribution from the light
+   */
+  calculateWidthContribution(light) {
+    const distance = dist(
+      this.cube.position.x,
+      this.cube.position.y,
+      light.x,
+      light.y
+    );
+
+    const maxDist = light.z;
+    const t = constrain(1 - distance / maxDist, 0, 1);
+    return lerp(this.baseWidth, light.z, t);
   }
 
   draw() {
--- src/ephemeral/sketch.mjs
+++ src/ephemeral/sketch.mjs
@@ -7,9 +7,11 @@ import { FloatingCube } from "./FloatingCube.js";
 const grid = [];
 
 /**
- * @type {Vector3}
+ * @type {Vector3[]}
  */
-const lightPos = new Vector3(0, 0, 100);
+const lights = [];
+
+const lightHeight = 100;
 
 const gridWidth = 50;
 const gridHeight = 50;
@@ -30,6 +32,15 @@ let zSize;
 const init = () => {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
+  lights.length = 0;
+  grid.length = 0;
+
+  lights.push(new Vector3(0, 0, lightHeight));
+  lights.push(new Vector3(-width, 0, lightHeight));
+  lights.push(new Vector3(width, 0, lightHeight));
+  lights.push(new Vector3(0, -height, lightHeight));
+  lights.push(new Vector3(0, height, lightHeight));
+
   xSize = width / gridWidth;
   ySize = height / gridHeight;
   zSize = 100;
@@ -48,18 +59,58 @@ const init = () => {
   }
 };
 
-const moveLight = () => {};
+const moveLights = () => {
+  const lightDeltaX = cos(noise(lights[0].x, lights[0].y, frameCount));
+  const lightDeltaY = sin(noise(lights[0].x, lights[0].y, frameCount));
+
+  lights[0].x += lightDeltaX;
+  lights[0].y += lightDeltaY;
+
+  if (lights[0].x > width) lights[0].x -= width;
+  if (lights[0].x < -width) lights[0].x += width;
+  if (lights[0].y > height) lights[0].y -= height;
+  if (lights[0].y < -height) lights[0].y += height;
+
+  for (let i = 1; i < lights.length; ++i) {
+    const mirrorX = lights[0].x;
+    const mirrorY = lights[0].y;
+
+    lights[i].x =
+      i === 1 ? mirrorX - width : i === 2 ? mirrorX + width : mirrorX;
+    lights[i].y =
+      i === 3 ? mirrorY - height : i === 4 ? mirrorY + height : mirrorY;
+  }
+};
 
 function setup() {
   init();
 }
 
 function draw() {
+  background(0);
+
+  lightFalloff(2.5, 0, 0);
+
+  for (let i = 0; i < lights.length; ++i) {
+    pointLight(
+      0,
+      255 - lerp(0, 230, sin(frameCount / 600)),
+      0,
+      lights[i].x - width / 2,
+      lights[i].y - height / 2,
+      lights[i].z
+    );
+  }
+
+  moveLights();
+
+  for (let i = 0; i < gridWidth; ++i) {
+    for (let j = 0; j < gridHeight; ++j) {
+      grid[i][j].update(lights);
+    }
+  }
+
   translate(-width / 2, -height / 2);
-
-  pointLight(0, 255, 0, lightPos.x, lightPos.y, lightPos.z);
-
-  moveLight();
 
   for (let i = 0; i < gridWidth; ++i) {
     for (let j = 0; j < gridHeight; ++j) {
